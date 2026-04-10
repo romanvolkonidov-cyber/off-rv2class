9141277eec64c5d2b9445ee0ecb895aa8f300d0b
Roman <h4m82gfhrk@privaterelay.appleid.com>
2026-04-10 13:53:52 +0300

    Hardcode backend IP in proxy to eliminate 502 Bad Gateway

--- client/next.config.ts
+++ client/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000'}/api/:path*`,
+        destination: 'http://158.220.94.77:4000/api/:path*', // Hardcode to be 100% sure
       },
     ];
   },
