7878479ce7175abf9947b36e4e4cce5306852b76
romanvolkonidov <romanvolkonidov@gmail.com>
2026-04-11 05:42:14 +0000

    k

--- client/src/lib/i18n.ts
+++ client/src/lib/i18n.ts
@@ -14,9 +14,29 @@ const resources = {
         email: "Email адрес",
         password: "Пароль",
         loginButton: "Войти в систему",
+        teacherRegistration: "Регистрация для учителей",
+        demoAccounts: "Демо-аккаунты:",
+        demoTeacher: "Учитель: teacher@rv2class.ru",
+        demoStudent: "Ученик: student@rv2class.ru",
+        demoPassword: "Пароль: 123456"
       },
       common: {
         loading: "Загрузка...",
+        create: "Создать",
+        delete: "Удалить",
+        save: "Сохранить",
+        cancel: "Отмена"
+      },
+      admin: {
+        title: "Панель администратора",
+        createCourse: "Создать курс",
+        createLesson: "Создать урок",
+        courseName: "Название курса",
+        courseDescription: "Описание курса",
+        lessonName: "Название урока",
+        uploadSlides: "Загрузить слайды",
+        publish: "Опубликовать",
+        unpublish: "Скрыть"
       }
     }
   }
